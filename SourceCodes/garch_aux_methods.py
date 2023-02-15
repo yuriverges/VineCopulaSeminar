3c55245731644ba7e8ee68ee39c0db475b5048ab
Yuri Verges <yuri.verges.academy@gmail.com>
2023-02-14 22:19:19 -0300

    Minor Fix - garch_aux_methods.py

--- SourceCodes/garch_aux_methods.py
+++ SourceCodes/garch_aux_methods.py
@@ -6,16 +6,16 @@ def zero_mean_garch_1_1_scenario(sim_std_resid, fitted_conditional_volatility, f
     sigma_2 = np.zeros(sim_std_resid.shape)
     eps_t = np.zeros(sim_std_resid.shape)
 
-    for t in range(sim_std_resid.shape):
+    for t in range(sim_std_resid.shape[0]):
         if t == 0:
             sigma_2[t] = w + alpha * np.power(fitted_residuals[-1], 2) + \
                              beta * np.power(fitted_conditional_volatility[-1], 2)
-            eps_t[t] = np.sqrt(sigma_2[t])
+            eps_t[t] = np.sqrt(sigma_2[t]) * sim_std_resid[t]
             r_t[t] = eps_t[t]
         else:
             sigma_2[t] = w + alpha * np.power(eps_t[t - 1], 2) + \
-                             beta * np.power(sigma_2[t - 1], 2)
-            eps_t[t] = np.sqrt(sigma_2[t])
+                             beta * sigma_2[t - 1]
+            eps_t[t] = np.sqrt(sigma_2[t]) * sim_std_resid[t]
             r_t[t] = eps_t[t]
 
-        return r_t, sigma_2, eps_t
+    return r_t, sigma_2, eps_t
